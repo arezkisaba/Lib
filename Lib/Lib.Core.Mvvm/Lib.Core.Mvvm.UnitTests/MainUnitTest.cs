7677fda6c8b92615abc1e8e2602fa3ac444531a2
Arezki Saba <saba.arezki@outlook.com>
2020-10-14 19:07:23 +0200

    ...

---
+++ Lib/Lib.Core.Mvvm/Lib.Core.Mvvm.UnitTests/MainUnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lib.Core.Mvvm.UnitTests
+{
+    [TestClass]
+    public class MainUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Lib/Lib.Core.OnlineServices.TeamFoundationServer/Lib.Core.OnlineServices.TeamFoundationServer.UnitTests/MainUnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lib.Core.OnlineServices.TeamFoundationServer.UnitTests
+{
+    [TestClass]
+    public class MainUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
