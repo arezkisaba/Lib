46a19766e45c36fa4217c99bd646d3d5684d2ea1
Arezki Saba <saba.arezki@outlook.com>
2020-10-25 17:18:50 +0100

    ...

--- Lib/Lib.Wix/Lib.Wix.Setup/update-productwxs.ps1
+++ Lib/Lib.Wix/Lib.Wix.Setup/update-productwxs.ps1
@@ -1,18 +1,17 @@
 ﻿$basePath = $PsScriptRoot
-$artifactFolderPath = "$basePath\Artifacts"
+$artifactFolderName = "Artifacts"
 $wxsTemplatePath = "$basePath\ProductTemplate.wxs"
 $wxsPath = "$basePath\Product.wxs"
-$items = Get-ChildItem $artifactFolderPath
+$items = Get-ChildItem "$basePath\$artifactFolderName"
 $xml = ""
 
 foreach ($item in $items) {
 	if (!$item.PSIsContainer) {
-		$absolutePath = "$artifactFolderPath\$($item.Name)"
-		$guid = (New-Guid).Guid;
+		$absolutePath = "$artifactFolderName\$($item.Name)"
 		$pattern = '[^a-zA-Z\._0-9]'
 		$fileId = $absolutePath.Replace($basePath, '').Replace('\', '_') -replace $pattern,''
 		$componentId = -join ((65..90) + (97..122) | Get-Random -Count 15 | % {[char]$_})
-		$row = "`t`t`t<Component Id=`"$componentId`" Guid=`"$guid`">`r`n`t`t`t`t<File Id=`"$fileId`" Name=`"$($item.Name)`" Source=`"$absolutePath`"/>`r`n`t`t`t</Component>`r`n"
+		$row = "`t`t`t<Component Id=`"$fileId`" Guid=`"*`">`r`n`t`t`t`t<File Id=`"$fileId`" Name=`"$($item.Name)`" Source=`"`$(var.ProjectDir)$absolutePath`"/>`r`n`t`t`t</Component>`r`n"
 		$xml += "$row`n"
 	}
 }
